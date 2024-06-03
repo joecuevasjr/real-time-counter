01f01fd08a54a9e88a2e8cd52341d9436cba7619
joecu <joecu@JOELAPTOP>
2024-06-03 17:21:33 -0500

    Add project files.

---
+++ ClassLibrary1/AwesomeAlgoClass.cs
@@ -0,0 +1,33 @@
+﻿namespace AwesomeAlgo
+{
+    public class AwesomeAlgoClass
+    {
+        public async IAsyncEnumerable<string> CountToUpperBoundAsync(int upperBound)
+        {
+
+            for (int i = 0; i < upperBound; i++)
+            {
+                var value = i + 1;
+
+                if (value % 3 == 0 && value % 5 != 0)
+                {
+                    yield return "Joe";
+                }
+                else if (value % 5 == 0 && value % 3 != 0)
+                {
+                    yield return "Cuevas";
+                }
+                else if (value % 5 == 0 && value % 3 == 0)
+                {
+                    yield return "Joe Cuevas";
+                }
+                else
+                {
+                    yield return value.ToString();
+                }
+
+                await Task.Delay(50); // Simulate delay
+            }
+        }
+    }
+}

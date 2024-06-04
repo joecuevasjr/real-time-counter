ef1e96add6ea70bb88d6c511617602a422b718ca
Jose Cuevas <joecuevasjr@gmail.com>
2024-06-03 22:42:10 -0500

    Change a file to use file scoped namespace

--- AwesomeAlgo/NumberNamePair.cs
+++ AwesomeAlgo/NumberNamePair.cs
@@ -1,24 +1,23 @@
-﻿namespace AwesomeAlgo
+﻿namespace AwesomeAlgo;
+
+public class NumberNamePair
 {
-    public class NumberNamePair
+    public int Number { get; }
+    public string Name { get; }
+
+    public NumberNamePair(int number, string name)
     {
-        public int Number { get; }
-        public string Name { get; }
+        // Ensuring the number is within the range 1 to Int32.MaxValue
+        if (number < 1 || number > int.MaxValue) 
+            throw new ArgumentOutOfRangeException(nameof(number), "Number must be between 1 and Int32.MaxValue.");
 
-        public NumberNamePair(int number, string name)
-        {
-            // Ensuring the number is within the range 1 to Int32.MaxValue
-            if (number < 1 || number > int.MaxValue) 
-                throw new ArgumentOutOfRangeException(nameof(number), "Number must be between 1 and Int32.MaxValue.");
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Name cannot be null or whitespace.", nameof(name));
 
-            if (string.IsNullOrWhiteSpace(name))
-                throw new ArgumentException("Name cannot be null or whitespace.", nameof(name));
+        if (!System.Text.RegularExpressions.Regex.IsMatch(name, @"^[a-zA-Z ]+$"))
+            throw new ArgumentException("Name must only contain alphabetical characters.");
 
-            if (!System.Text.RegularExpressions.Regex.IsMatch(name, @"^[a-zA-Z ]+$"))
-                throw new ArgumentException("Name must only contain alphabetical characters.");
-
-            Number = number;
-            Name = name;
-        }
+        Number = number;
+        Name = name;
     }
 }
